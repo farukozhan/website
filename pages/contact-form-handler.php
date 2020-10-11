8cb1b0884c755dd2eab399b45b1053663983b624
farukozhan <65257604+farukozhan@users.noreply.github.com>
2020-10-11 19:00:56 -0400

    Update contact-form-handler.php

--- pages/contact-form-handler.php
+++ pages/contact-form-handler.php
@@ -3,15 +3,15 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-$email_from = 'info@theimperialtrade.com';
+$email_from = 'farukozhan@yahoo.com';
 $email_subject = "New Form Submission";
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
                     "User Message: $message.\n";
-$to = "farukozhan@yahoo.com";
+$to = "info@theimperialtrade.com";
 $headers = " From: $email_from\r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
-mail($to,$email_subject,$email_body,&headers);
+mail($to,$email_subject,$email_body,$headers);
 header("Location: contact.html");
 
 ?>
