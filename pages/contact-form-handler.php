6e7d59385da5c4d8bf9bf7e62b49ea39b456663e
farukozhan <65257604+farukozhan@users.noreply.github.com>
2020-10-11 19:18:50 -0400

    Update contact-form-handler.php

--- pages/contact-form-handler.php
+++ pages/contact-form-handler.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-$email_from = 'farukozhan@yahoo.com';
+$email_from = "farukozhan@yahoo.com";
 $email_subject = "New Form Submission";
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
